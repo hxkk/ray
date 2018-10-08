eac59788dd80711bd9f449b7a922e127a5e86fc9
mpadmin <hxkk.kim@medipixel.io>
2018-10-08 17:39:25 +0900

    BCO agent version0.1: 1. fix error

--- python/ray/rllib/agents/bco/inverse_dynamics_model.py
+++ python/ray/rllib/agents/bco/inverse_dynamics_model.py
@@ -9,70 +9,11 @@ import ray
 from ray.rllib.models import ModelCatalog
 
 from ray.rllib.models.fcnet import FullyConnectedNetwork
+from ray.rllib.agents.bco.input_filter import input_filter
 
 import numpy
 import pandas
 import pickle
-
-# STATE_SIZE = 17
-# STATE_SIZE = 51
-STATE_SIZE = 175
-
-
-# STATE_SIZE = 249
-
-def env_creator(env_config):
-    env = ProstheticsEnv(False)
-    #     env = gym.make("CartPole-v0")
-    return env
-
-
-config = {
-    # Discount factor
-    "gamma": 0.998,
-    # If true, use the Generalized Advantage Estimator (GAE)
-    # with a value function, see https://arxiv.org/pdf/1506.02438.pdf.
-    "use_gae": True,
-    # Number of workers
-    # "num_workers": 0, # 72 * 3 + 5
-    "num_workers": 7,  # 72 * 3 + 5
-    # GAE(lambda) parameter
-    "lambda": 0.95,
-    # Initial coefficient for KL divergence
-    "num_sgd_iter": 10,
-    # Stepsize of SGD
-    "sgd_stepsize": 3e-4,
-    # batch_size
-    "sample_batch_size": 128,
-    # Coefficient of the value function loss
-    "vf_loss_coeff": 1.0,
-    # Coefficient of the entropy regularizer
-    "entropy_coeff": 0.001,
-    # PPO clip parameter
-    "clip_param": 0.2,
-    # Target value for KL divergence
-    "kl_target": 0.01,
-    # Number of GPUs to use for SGD
-    "num_gpus": 1,
-    # Whether to allocate GPUs for workers (if > 0).
-    "num_gpus_per_worker": 0,
-    # Whether to allocate CPUs for workers (if > 0).
-    "num_cpus_per_worker": 1,
-    # Whether to rollout "complete_episodes" or "truncate_episodes"
-    "batch_mode": "complete_episodes",
-    # Which observation filter to apply to the observation
-    # "observation_filter": "MeanStdFilter",
-    # "observation_filter": "NoFilter",
-    # Use the sync samples optimizer instead of the multi-gpu one
-    "simple_optimizer": True,
-    "demo_file_path": "/data/nips/demos/pros_obs169_181008.xlsx",
-    "env_config": {},
-    "model": {},
-    "grad_clip": 40.0,
-    "batch_size": 100,
-    "lr": 0.0001,
-    "ext_data_path": "",
-}
 
 class InverseDynamicsModel(object):
     def __init__(self, env_creator, config, is_ext_train=False):
@@ -122,10 +63,10 @@ class InverseDynamicsModel(object):
         self.loss = self.pi_loss
         self.optimizer = tf.train.AdamOptimizer(self.config["lr"]).minimize(self.loss)
 
-        # Setup accuracy -> cosine similarity
+        # Setup similarity -> cosine similarity
         normalize_sample = tf.nn.l2_normalize(self.sample, 1)
         normalize_ac = tf.nn.l2_normalize(self.ac, 1)
-        self.accuracy = 1 - tf.losses.cosine_distance(normalize_sample, normalize_ac, dim=1)
+        self.similarity = 1 - tf.losses.cosine_distance(normalize_sample, normalize_ac, dim=1)
 
         # Initialize
         self.initialize()
@@ -184,7 +125,7 @@ class InverseDynamicsModel(object):
         return actions
 
     def _load_demonstration(self):
-        df = pandas.read_excel(self.config["demo_file_path"])
+        df = pandas.read_excel(self.config["invese_model"]["demo_file_path"])
         df = df.drop(df.columns[0], axis=1)
 
         self.demo = df.as_matrix()
@@ -236,14 +177,12 @@ class InverseDynamicsModel(object):
         BASE_PATH = "/data/nips/es_trans_data/"
 
         #         train_df = pandas.read_csv("/data/nips/train_list/train_201810061612.csv")
-        train_df = pandas.read_csv("/data/nips/train_list/train_201810042003.csv")
+        train_df = pandas.read_csv(self.config["invese_model"]["ext_train_file_path"])
         train_obs_data = []
         train_act_data = []
         for data in train_df["name"]:
             obs = numpy.load(BASE_PATH + data + "_obs.npy").astype(numpy.float32)
-            #             obs_t = obs[:(obs.shape[0]-1), :STATE_SIZE]
-            #             obs_next_t = obs[1:, :STATE_SIZE]
-            obs = self.input_filter(obs)
+            obs = input_filter(obs)
             obs_t = obs[:(obs.shape[0] - 1), :]
             obs_next_t = obs[1:, :]
             obs_transitioins = numpy.concatenate((obs_t, obs_next_t), axis=1)
@@ -258,14 +197,12 @@ class InverseDynamicsModel(object):
         train_dataset = train_dataset.map(map_batch)
 
         #         valid_df = pandas.read_csv("/data/nips/train_list/valid_201810061612.csv")
-        valid_df = pandas.read_csv("/data/nips/train_list/valid_201810042003.csv")
+        valid_df = pandas.read_csv(self.config["invese_model"]["ext_valid_file_path"])
         valid_obs_data = []
         valid_act_data = []
         for data in train_df["name"]:
             obs = numpy.load(BASE_PATH + data + "_obs.npy").astype(numpy.float32)
-            #             obs_t = obs[:(obs.shape[0]-1), :STATE_SIZE]
-            #             obs_next_t = obs[1:, :STATE_SIZE]
-            obs = self.input_filter(obs)
+            obs = input_filter(obs)
             obs_t = obs[:(obs.shape[0] - 1), :]
             obs_next_t = obs[1:, :]
             obs_transitioins = numpy.concatenate((obs_t, obs_next_t), axis=1)
@@ -291,23 +228,23 @@ class InverseDynamicsModel(object):
 
         for epoch in range(epochs):
             for i in range(train_steps):
-                l, _, acc = self.sess.run([self.loss, self.optimizer, self.accuracy])
+                l, _, acc = self.sess.run([self.loss, self.optimizer, self.similarity])
                 if i % (train_steps / 10) == 0:
-                    print("Epoch: {}, train step: {}, loss: {:.3f}, training accuracy: {:.2f}%".format(epoch, i, l,
+                    print("Epoch: {}, train step: {}, loss: {:.3f}, training similarity: {:.2f}%".format(epoch, i, l,
                                                                                                        acc * 100))
             #                     print("train step: {}, loss: {:.3f}".format(i, l))
 
             avg_acc = 0
             valid_loss_str = ""
             for i in range(valid_iters):
-                acc = self.sess.run([self.accuracy])
+                acc = self.sess.run([self.similarity])
                 avg_acc += acc[0]
-            print("Average validation set accuracy over {} iterations is {:.2f}%".format(valid_iters,
+            print("Average validation set similarity over {} iterations is {:.2f}%".format(valid_iters,
                                                                                          (avg_acc / valid_iters) * 100))
 
             env_mdoel_data = self.get_weights()
             #             pickle.dump(env_mdoel_data, open("/data/nips/ckpt/env_mdoel_data_"+str(epochs), "wb"))
-            pickle.dump(env_mdoel_data, open("/data/nips/ckpt/env_mdoel_data.test" + str(STATE_SIZE), "wb"))
+            pickle.dump(env_mdoel_data, open(self.config["invese_model"]["ext_output_path"], "wb"))
 
     def test_model(self):
         demo_transitioins = self._get_demo_transitions()
@@ -315,20 +252,6 @@ class InverseDynamicsModel(object):
         infer_actions = self.compute_actions(demo_transitioins)
 
         return infer_actions
-
-    def input_filter(self, obs):
-        # 412 -> 175
-        #         new_obs += obs[0:33+1] + obs[51:116+1] + obs[150:215+1] + obs[403:411+1]
-#         new_obs = obs[:, numpy.r_[0:33 + 1, 51:116 + 1, 150:215 + 1, 403:411 + 1]]
-        # 412 -> 169
-#         new_obs += obs[0:7 + 1] + obs[9:12 + 1] + obs[14:15 + 1] \
-#             + obs[17:24 + 1] + obs[26:29 + 1] + obs[31:32 + 1] \
-#             + obs[51:116 + 1] + obs[150:215 + 1] + obs[403:411 + 1]
-        new_obs = obs[:, numpy.r_[0:7 + 1, 9:12 + 1, 14:15 + 1,
-                                  17:24 + 1, 26:29 + 1, 31:32 + 1,
-                                  51:116 + 1, 150:215 + 1, 403:411 + 1]]
-
-        return new_obs
 
     def temp_test(self):
         epochs = 100
@@ -340,7 +263,7 @@ class InverseDynamicsModel(object):
 
         for epoch in range(epochs):
             for i in range(train_steps):
-                l, acc = self.sess.run([self.loss, self.accuracy])
+                l, acc = self.sess.run([self.loss, self.similarity])
                 if i % (train_steps / 10) == 0:
-                    print("Epoch: {}, train step: {}, loss: {:.3f}, training accuracy: {:.2f}%".format(epoch, i, l,
+                    print("Epoch: {}, train step: {}, loss: {:.3f}, training similarity: {:.2f}%".format(epoch, i, l,
                                                                                                        acc * 100))
